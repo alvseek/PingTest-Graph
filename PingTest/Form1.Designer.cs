def1b7af23012d1e978420b6821ec57d47c8127f
Alv <Alv@ALV>
2018-04-18 13:46:38 +0700

    Add project files.

---
+++ PingTest/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace PingTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            this.PingLabel = new System.Windows.Forms.Label();
+            this.PingChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.PingChart)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PingLabel
+            // 
+            this.PingLabel.BackColor = System.Drawing.Color.Transparent;
+            this.PingLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.PingLabel.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PingLabel.ForeColor = System.Drawing.Color.White;
+            this.PingLabel.Location = new System.Drawing.Point(0, 44);
+            this.PingLabel.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.PingLabel.Name = "PingLabel";
+            this.PingLabel.Padding = new System.Windows.Forms.Padding(2, 0, 0, 3);
+            this.PingLabel.Size = new System.Drawing.Size(120, 17);
+            this.PingLabel.TabIndex = 0;
+            this.PingLabel.Text = "-";
+            this.PingLabel.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // PingChart
+            // 
+            this.PingChart.BackColor = System.Drawing.Color.Transparent;
+            this.PingChart.BorderlineColor = System.Drawing.Color.Transparent;
+            this.PingChart.BorderlineWidth = 0;
+            this.PingChart.BorderSkin.BorderWidth = 0;
+            chartArea1.AxisX.IsLabelAutoFit = false;
+            chartArea1.AxisX.IsMarginVisible = false;
+            chartArea1.AxisX.LabelStyle.Enabled = false;
+            chartArea1.AxisX.LabelStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.AxisX.LabelStyle.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.AxisX.LineColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisX.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartArea1.AxisX.MajorGrid.Enabled = false;
+            chartArea1.AxisX.MajorTickMark.Enabled = false;
+            chartArea1.AxisX.TitleForeColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisX2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.AxisX2.TitleForeColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisY.IsLabelAutoFit = false;
+            chartArea1.AxisY.IsMarginVisible = false;
+            chartArea1.AxisY.LabelAutoFitStyle = ((System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles)(((System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.DecreaseFont | System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.StaggeredLabels) 
+            | System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.WordWrap)));
+            chartArea1.AxisY.LabelStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.AxisY.LabelStyle.ForeColor = System.Drawing.Color.White;
+            chartArea1.AxisY.LabelStyle.Format = "D";
+            chartArea1.AxisY.LabelStyle.Interval = 0D;
+            chartArea1.AxisY.LineColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisY.MajorGrid.Enabled = false;
+            chartArea1.AxisY.MajorTickMark.Enabled = false;
+            chartArea1.AxisY.MajorTickMark.LineColor = System.Drawing.Color.Silver;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.AxisY2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.BackImageTransparentColor = System.Drawing.Color.Transparent;
+            chartArea1.BackSecondaryColor = System.Drawing.Color.Transparent;
+            chartArea1.BorderWidth = 0;
+            chartArea1.InnerPlotPosition.Auto = false;
+            chartArea1.InnerPlotPosition.Height = 52F;
+            chartArea1.InnerPlotPosition.Width = 65F;
+            chartArea1.InnerPlotPosition.X = 27F;
+            chartArea1.InnerPlotPosition.Y = 13F;
+            chartArea1.Name = "ChartArea1";
+            chartArea1.Position.Auto = false;
+            chartArea1.Position.Height = 100F;
+            chartArea1.Position.Width = 100F;
+            this.PingChart.ChartAreas.Add(chartArea1);
+            this.PingChart.Enabled = false;
+            this.PingChart.Location = new System.Drawing.Point(0, 0);
+            this.PingChart.Margin = new System.Windows.Forms.Padding(0);
+            this.PingChart.Name = "PingChart";
+            this.PingChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Grayscale;
+            this.PingChart.Size = new System.Drawing.Size(62, 61);
+            this.PingChart.SuppressExceptions = true;
+            this.PingChart.TabIndex = 1;
+            this.PingChart.Text = "chart1";
+            this.PingChart.Visible = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(117, 54);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(113, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(120, 61);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.ControlBox = false;
+            this.Controls.Add(this.PingLabel);
+            this.Controls.Add(this.PingChart);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(16, 39);
+            this.Name = "Form1";
+            this.Opacity = 0.5D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.PingChart)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PingLabel;
+        private System.Windows.Forms.DataVisualization.Charting.Chart PingChart;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ PingTest/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace PingTest
+{
+    public partial class Form1 : Form
+    {
+        private AutoResetEvent waiter = new AutoResetEvent(false);
+
+        private const int WM_NCHITTEST = 0x84;
+        private const int HT_CLIENT = 0x1;
+        private const int HT_CAPTION = 0x2;
+        private const int WM_NCRBUTTONDOWN = 0xa4;
+        Series newSeries = new Series();
+
+        static string IPTarget = "8.8.8.8";
+        Ping pingSender = new Ping();
+        IPAddress address = IPAddress.Parse(IPTarget);
+
+        delegate void SetTextCallback(string text);
+        delegate void SetChartCallback();
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == WM_NCHITTEST)
+                m.Result = (IntPtr)(HT_CAPTION);
+            
+            if (m.Msg == WM_NCRBUTTONDOWN)
+            {
+                var pos = new Point(m.LParam.ToInt32());
+                OnTitlebarClick(pos);
+            }           
+        }
+
+        protected void OnTitlebarClick(Point pos)
+        {
+            contextMenuStrip1.Show(pos);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            MakeSeries();
+            ThreadPool.QueueUserWorkItem(o => DoPingAsync(IPTarget));
+        }
+
+        private void MakeSeries()
+        {
+            Console.WriteLine("we get in?");
+            
+            PingChart.Series.Add(newSeries);
+            newSeries.ChartArea = "ChartArea1";
+            newSeries.ChartType = SeriesChartType.Line;
+            newSeries.Color = Color.White;
+            newSeries.MarkerColor = Color.White;
+            newSeries.MarkerStyle = MarkerStyle.Circle;
+            newSeries.MarkerSize = 1;
+        }
+
+        private void DoPingAsync(string args)
+        {
+
+            if (args.Length == 0)
+                throw new ArgumentException("Ping needs a host or IP Address.");
+
+            string who = args;
+
+
+
+            Ping pingSender = new Ping();
+
+            // When the PingCompleted event is raised,
+            // the PingCompletedCallback method is called.
+            pingSender.PingCompleted += new PingCompletedEventHandler(PingCompletedCallback);
+
+            /*
+            // Create a buffer of 1 bytes of data to be transmitted.
+            string data = "1";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            */
+            // Wait 5 seconds for a reply.
+            int timeout = 5000;
+
+            // Set options for transmission:
+            // The data can go through 64 gateways or routers
+            // before it is destroyed, and the data packet
+            // cannot be fragmented.
+            /*
+            PingOptions options = new PingOptions(64, true);
+
+            Console.WriteLine("Time to live: {0}", options.Ttl);
+            Console.WriteLine("Don't fragment: {0}", options.DontFragment);
+            */
+            // Send the ping asynchronously.
+            // Use the waiter as the user token.
+            // When the callback completes, it can wake up this thread.
+            //Trace.WriteLine("Ping example started.");
+
+            SetChart();
+
+            do
+            {
+                Thread.Sleep(1000);
+                pingSender.SendAsync(who, timeout, waiter);
+                //Console.WriteLine("waits on AutoResetEvent");
+                waiter.WaitOne();
+            } while (true);
+            //Console.WriteLine("Ping example completed.");
+        }
+
+        private void PingCompletedCallback(object sender, PingCompletedEventArgs e)
+        {
+            // If the operation was canceled, display a message to the user.
+            if (e.Cancelled)
+            {
+                Console.WriteLine("Ping canceled.");
+
+                // Let the main thread resume. 
+                // UserToken is the AutoResetEvent object that the main thread 
+                // is waiting for.
+                ((AutoResetEvent)e.UserState).Set();
+            }
+
+            // If an error occurred, display the exception to the user.
+            if (e.Error != null)
+            {
+                Console.WriteLine("Ping failed:");
+                Console.WriteLine(e.Error.ToString());
+
+                // Let the main thread resume. 
+                ((AutoResetEvent)e.UserState).Set();
+            }
+
+            PingReply reply = e.Reply;
+
+            DisplayReply(reply);
+
+            // Let the main thread resume.
+            ((AutoResetEvent)e.UserState).Set();
+        }
+
+        public void DisplayReply(PingReply reply)
+        {
+            if (reply == null)
+                return;
+
+            //Console.WriteLine("ping status: {0}", reply.Status.ToString());
+            if (reply.Status == IPStatus.Success)
+            {
+                SetText(reply.RoundtripTime.ToString());
+            }
+            else
+            {
+                string statusCode = reply.Status.ToString();
+                if (statusCode == "11050") statusCode = "Error";
+                SetText(statusCode);
+            }
+        }
+
+        private void SetChart()
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.PingChart.InvokeRequired)
+            {
+                SetChartCallback d = new SetChartCallback(SetChart);
+                this.Invoke(d, new object[] {});
+            }
+            else
+            {
+                this.PingChart.Visible = true;
+            }
+        }
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.PingLabel.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.PingLabel.Text = text;
+                if (newSeries.Points.Count >= 10)
+                {
+                    newSeries.Points.RemoveAt(0);
+                }
+                try
+                {
+                    int pingTime = int.Parse(text);
+
+                    newSeries.Points.AddXY(DateTime.Now, pingTime);
+
+                    if (pingTime <= 300)
+                    {
+                        this.PingLabel.ForeColor = Color.White;
+                        //Console.WriteLine("Warna putih " + PingLabel.ForeColor);
+                        this.newSeries.Points[newSeries.Points.Count - 1].Color = Color.White;
+                    }
+                    else if (pingTime >= 300 && pingTime < 1000)
+                    {
+                        this.PingLabel.ForeColor = Color.Orange;
+                        //Console.WriteLine("Warna oren " + PingLabel.ForeColor);
+                        this.newSeries.Points[newSeries.Points.Count - 1].Color = Color.Orange;
+                    }
+                    else
+                    {
+                        this.PingLabel.ForeColor = Color.Red;
+                        //Console.WriteLine("Warna mewah " + PingLabel.ForeColor);
+                        this.newSeries.Points[newSeries.Points.Count - 1].Color = Color.Red;
+                    }
+
+                    this.PingLabel.Text += " ms";
+       
+                    PingChart.ChartAreas[0].AxisX.Maximum = newSeries.Points[newSeries.Points.Count - 1].XValue;
+                    PingChart.ChartAreas[0].AxisX.Minimum = newSeries.Points[0].XValue;
+                    PingChart.ChartAreas[0].AxisY.Maximum = newSeries.Points.Max(y => y.YValues[0]);
+                    PingChart.ChartAreas[0].AxisY.Minimum = newSeries.Points.Min(y => y.YValues[0]);
+                    double axisInterval;
+                    if (PingChart.ChartAreas[0].AxisY.Maximum - PingChart.ChartAreas[0].AxisY.Minimum <= 3)
+                    {
+                        PingChart.ChartAreas[0].AxisY.Maximum += 3;
+                    }
+                    axisInterval = (PingChart.ChartAreas[0].AxisY.Maximum - PingChart.ChartAreas[0].AxisY.Minimum) / 3.3;
+                    PingChart.ChartAreas[0].AxisY.LabelStyle.Interval = axisInterval;
+                }
+                catch
+                {
+                    this.PingLabel.ForeColor = Color.Red;
+                    if (newSeries.Points.Count != 0)
+                    {
+                        newSeries.Points.AddXY(DateTime.Now, newSeries.Points[newSeries.Points.Count - 1].YValues[0]);
+                    }
+                    else newSeries.Points.AddXY(DateTime.Now, 0);
+                    this.newSeries.Points[newSeries.Points.Count - 1].Color = Color.Transparent;
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
