d60f26c3ba047eb0028c2d448973277c3b6836db
alv <alviandi@msn.com>
2018-08-13 02:51:19 +0700

    -fixed window size

--- PingTest/Donation.Designer.cs
+++ PingTest/Donation.Designer.cs
@@ -92,7 +92,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(335, 220);
+            this.ClientSize = new System.Drawing.Size(335, 219);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
